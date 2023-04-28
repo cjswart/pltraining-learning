d5c223ea67df2d78c7423d4f90f8489a2d093ac7
Cees-Jan Swart <cees-jan.swart@kpn.com>
2023-04-28 15:19:07 +0200

    Update dockeragent.pp

--- manifests/dockeragent.pp
+++ manifests/dockeragent.pp
@@ -1,7 +1,7 @@
 class learning::dockeragent {
   # Run the dockeragent class to pre-install images
   class { "dockeragent":
-    create_no_agent_image => true,
+    #create_no_agent_image => true,
     lvm_bashrc            => true,
   }
 }
