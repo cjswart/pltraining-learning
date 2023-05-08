d9d37892fd4f97cf0c4bc077afb33b5fd359138b
Cees-Jan Swart <cees-jan.swart@kpn.com>
2023-05-08 09:07:10 +0200

    Create gitea.pp

---
+++ manifests/gitea.pp
@@ -0,0 +1,16 @@
+# install gitea
+class profile::gitea {
+  include mysql::server
+  mysql::db { 'gitea':
+    ensure   => present,
+    user     => 'gitea',
+    password => 'time2value',
+    host     => '127.0.0.1',
+    grant    => [ 'ALL' ],
+  }
+  class {'gitea':
+    version => '1.6.0',
+    checksum => '9c66d4207b49309de9d4d750bab1090c15bf6de5d99e0de819f5215cba35d2cb',
+  }
+
+}
